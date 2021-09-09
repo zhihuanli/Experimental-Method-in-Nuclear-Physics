f8be2ef169a60338d40b134a56b6b80a7d0be570
Test <zhli@pku.edu.cn>
2021-09-09 11:01:22 +0800

    Add files via upload

--- chapt1/coursework1.1/lisec.C
+++ chapt1/coursework1.1/lisec.C
@@ -1,31 +1,37 @@
+
+TGraph* GetdEdxLise(string elossfile)
+{
+    ifstream inf(elossfile);//读入LISE软件生成的12C入射到H2O的数据
+    if(!inf.is_open()){     //文件不存时。。
+        cout<<"Data File: "<<elossfile<< " does not exist!"<<endl;
+        return 0;
+    }
+    auto *g=new TGraph;//定义新的二维散点图(TGraph) g
+    string ss;
+    double a, b, e, dedx;    
+    getline(inf,ss);//读入第一行
+    int i=0;
+    while(inf >> a >> b >> e >> dedx >> a >> b >> a >> b >> a >> b >> a >> b) 
+    {
+        g->SetPoint(i++, e*12,dedx*1000);//添加数据点到图中，能量单位为MeV/u，对C需乘以12.
+    }
+    inf.close();
+    return g;
+}
+
 TCanvas *c1;
 TGraph *g1;
 
 void lisec(){
-  ifstream inf("12C-water.txt");
- string ss;
- double a, b, e, dedx;
- g1=new TGraph;//定义新的二维散点图(TGraph) g1
- if(!inf.is_open()){//文件不存在则退出程序
-   cout<<"Data File does not exist!"<<endl;
-   return 0;
- }
-
- getline(inf,ss);
- int i=0;
- while(!inf.eof())
-   {
-     inf >> a >>b>>e>>dedx>>a>>b>>a>>b>>a>>b>>a>>b;
-     // 对照数据文件进行理解。一列一列地读入，提取第3、4列数据。
-     g1->SetPoint(i++, e*12,dedx);//添加数据点到图中，能量单位为MeV/u，对C需乘以12.
-   }
-
- inf.close();
- g1->SetTitle("dE/dx(MeV/um) vs. MeV for 12C in water");//设定图的标题
- c1=new TCanvas("c1","c1");
- gPad->SetLogy();
- gPad->SetLogx();
- g1->Draw();
-
+  string elossfile="12C-water.txt";
+  TGraph *g=GetdEdxLise(elossfile);
+  if(!g) return;
+    g->SetTitle("dE/dx(MeV/mm) vs. MeV for 12C in water");//设定图的标题
+    c1=new TCanvas("c1","c1");
+    gPad->SetLogy();
+    gPad->SetLogx();
+    g->SetLineColor(kGreen);
+    g->Draw();
+    cout<<g->Eval(0.15)<<endl;
 }
 
--- chapt1/coursework1.1/srimc.C
+++ chapt1/coursework1.1/srimc.C
@@ -1,36 +1,45 @@
+
+TGraph* GetdEdxSrim(string elossfile)
+{
+
+    ifstream inf(elossfile);//读入LISE软件生成的12C入射到H2O的数据
+    if(!inf.is_open()){     //文件不存时。。
+        cout<<"Data File: "<<elossfile<< " does not exist!"<<endl;
+        return 0;
+    }
+    auto *g=new TGraph;//定义新的二维散点图(TGraph) g
+    string ss;
+    string sunit;//能量单位
+    double a, e,ededx, ndedx;//ededx-电子阻止本领，ndedx-核阻止本领   
+    int i=0;
+    for(int j=0;j<24;j++) getline(inf,ss);//读入文件中开始的24行描述性文本
+    while(inf >> e >> sunit >> ededx >> ndedx >> a >> ss >> a >> ss >> a >> ss) 
+    {
+       if(abs(e)<1.0e-4) break;//对于非数据，e值为零，此时停止读数据
+        if(sunit=="keV") e /=1e3;
+        if(sunit=="GeV") e *= 1e3;
+        g->SetPoint(i++, e, ededx+ndedx);//在g1中加入点, 参考TGraph class reference 中 SetPoint()函数用法
+
+    }
+    inf.close();
+    return g;
+}
+
 TCanvas *c1;
 TGraph *g1;//定义新的二维散点图(TGraph) g1
 
 void srimc()
 {
-  g1=new TGraph;
-  string ss, sunit;
-  double a, b, e, ededx, ndedx;//ededx-电子阻止本领，ndedx-核阻止本领
-  ifstream inf("12C-water-srim.txt");//读入Srim2013程序生成的12C入射H2O的输出文件
-  if(!inf.is_open()){//文件不存在则退出程序
-      cout<<"Data File does not exist!"<<endl;
-      return 0;
-  }
-  for(int j=0;j<24;j++) {
-    getline(inf,ss);//读入文件中开始的24行描述性文本
-    // cout<<ss<<endl;
-  } 
-  int i=0;
-  while(i<10000)
-    {
-      inf >> e >> sunit >> ededx >> ndedx >> a >> ss >> a >> ss >> a >> ss;
-      if(abs(e)<1.0e-4) break;//对于非数据，e值为零，此时停止读数据
-      if(sunit=="keV") e /=1e3;
-      if(sunit=="GeV") e *= 1e3;
-      g1->SetPoint(i, e, ededx+ndedx);//在g1中加入点, 参考TGraph class reference 中 SetPoint()函数用法
-      i++;
-    }
- 
-  inf.close();
-  g1->SetLineColor(kGreen);//设置g1显示线的颜色为绿色
+  string elossfile="12C-water-srim.txt";
+  g1=GetdEdxSrim(elossfile);
+  if(!g1) return;
+  g1->SetTitle("dE/dx(MeV/mm) vs. MeV for 12C in water");//设定图的标题
+  g1->SetLineColor(kGreen);
   c1=new TCanvas("c1","c1");
   gPad->SetLogy();
   gPad->SetLogx();
-  g1->Draw();//画g1图
-  
+  g1->Draw();
+  cout<<g1->Eval(0.15)<<endl;
 }
+  
+ 
