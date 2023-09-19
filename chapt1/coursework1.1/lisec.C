86f3a146768d6fd83ec812f9630391db10436da0
Test <zhli@pku.edu.cn>
2023-09-19 10:55:36 +0800

    Add files via upload

--- chapt1/coursework1.1/lisec.C
+++ chapt1/coursework1.1/lisec.C
@@ -1,22 +1,27 @@
 
-TGraph* GetdEdxLise(string elossfile)
+TGraph* GetdEdxLise(string elossfile,int A)
 {
-    ifstream inf(elossfile);//读入LISE软件生成的12C入射到H2O的数据
-    if(!inf.is_open()){     //文件不存时。。
-        cout<<"Data File: "<<elossfile<< " does not exist!"<<endl;
-        return 0;
-    }
-    auto *g=new TGraph;//定义新的二维散点图(TGraph) g
-    string ss;
-    double a, b, e, dedx;    
-    getline(inf,ss);//读入第一行
-    int i=0;
-    while(inf >> a >> b >> e >> dedx >> a >> b >> a >> b >> a >> b >> a >> b) 
+  ifstream inf(elossfile);//读入LISE软件生成的数据文件
+  if(!inf.is_open()){     //文件不存时
+    cout<<"Data File: "<<elossfile<< " does not exist!"<<endl;
+    return 0;
+  }
+  auto *g=new TGraph;//定义新的二维散点图(TGraph) g
+  string ss;
+  double a, b, e, dedx;    
+  getline(inf,ss);//读入第一行
+  int i=0;
+
+  while(!inf.eof())
     {
-        g->SetPoint(i++, e*12,dedx*1000);//添加数据点到图中，能量单位为MeV/u，对C需乘以12.
+      inf >> a >> b >> e >> dedx;
+      getline(inf,ss);//读取每一行剩余的数据，包括最后的空格
+      if(inf.eof()) break;//避免最后一个重复读取
+      g->SetPoint(i++, e*A,dedx*1000);//添加数据点到图中，能量单位为MeV/u，需乘以核子数 A
     }
-    inf.close();
-    return g;
+    
+  inf.close();
+  return g;
 }
 
 TCanvas *c1;
--- chapt1/coursework1.1/srimc.C
+++ chapt1/coursework1.1/srimc.C
@@ -1,9 +1,7 @@
-
 TGraph* GetdEdxSrim(string elossfile)
 {
-
-    ifstream inf(elossfile);//读入LISE软件生成的12C入射到H2O的数据
-    if(!inf.is_open()){     //文件不存时。。
+   ifstream inf(elossfile);//读入LISE软件生成的数据
+    if(!inf.is_open()){     //文件不存时
         cout<<"Data File: "<<elossfile<< " does not exist!"<<endl;
         return 0;
     }
@@ -13,16 +11,18 @@ TGraph* GetdEdxSrim(string elossfile)
     double a, e,ededx, ndedx;//ededx-电子阻止本领，ndedx-核阻止本领   
     int i=0;
     for(int j=0;j<24;j++) getline(inf,ss);//读入文件中开始的24行描述性文本
-    while(inf >> e >> sunit >> ededx >> ndedx >> a >> ss >> a >> ss >> a >> ss) 
-    {
-       if(abs(e)<1.0e-4) break;//对于非数据，e值为零，此时停止读数据
+    while(!inf.eof()) 
+      {
+        inf >> e >> sunit >> ededx >> ndedx;
+        getline(inf,ss);//读取每一行剩余的数据，包括最后的空格
+        if(abs(e)<1.0e-4) break;//对于非数据，e值为零，此时停止读数据
         if(sunit=="keV") e /=1e3;
         if(sunit=="GeV") e *= 1e3;
         g->SetPoint(i++, e, ededx+ndedx);//在g1中加入点, 参考TGraph class reference 中 SetPoint()函数用法
-
-    }
+      }
     inf.close();
     return g;
+}
 }
 
 TCanvas *c1;
